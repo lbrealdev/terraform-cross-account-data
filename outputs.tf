0009c850d9d344fa781ccf3194e01e86e5e484fe
lbrealdev <lbrealdeveloper@gmail.com>
2026-04-27 10:23:53 +0200

    fix: remove sensitive from outputs

--- outputs.tf
+++ outputs.tf
@@ -14,25 +14,21 @@ output "account1_account_id" {
 output "account1_main_vpc" {
   description = "Main VPC ID from Account 1"
   value       = data.aws_vpc.account1_main.id
-  sensitive   = true
 }
 
 output "account1_public_subnets" {
   description = "List of public subnets from Account 1"
   value       = data.aws_subnet.account1_public[*].id
-  sensitive   = true
 }
 
 output "account1_all_subnets" {
   description = "All subnets from Account 1"
   value       = data.aws_subnets.account1_all.ids
-  sensitive   = true
 }
 
 output "account1_web_security_groups" {
   description = "Security groups from Account 1 with Environment=production and Name=web-*"
   value       = data.aws_security_groups.account1_web.ids
-  sensitive   = true
 }
 
 # ============================================
@@ -47,5 +43,4 @@ output "account2_account_id" {
 output "account2_db_security_groups" {
   description = "Security groups from Account 2 with Environment=production and Name=db-*"
   value       = data.aws_security_groups.account2_db.ids
-  sensitive   = true
 }
